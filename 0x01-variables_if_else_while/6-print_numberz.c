b826286533aa76ab4c8ddd9aaddf4ce020722a36
wagiih <m.wagih.raafat@gmail.com>
2023-07-14 12:47:54 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: Always (0) success
+*/
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
