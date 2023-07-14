0bff78e6b9c272d93c7bd5f8b2fa3388bed99470
wagiih <m.wagih.raafat@gmail.com>
2023-07-14 13:02:02 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: Always (0) success
+*/
+int main(void)
+{
+	int n = 48;
+	int m = 97;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 102)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
