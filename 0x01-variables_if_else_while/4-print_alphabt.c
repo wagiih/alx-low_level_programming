57225fe3f3ebbd583766ed5eb80d852815454388
wagiih <m.wagih.raafat@gmail.com>
2023-07-14 09:54:02 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: Always (0) success
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 113 || n == 101)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
