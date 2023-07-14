ccc277545151c03f5e65a6c4376dd24c3efba97c
wagiih <m.wagih.raafat@gmail.com>
2023-07-14 13:19:36 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: Always (0) success
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
