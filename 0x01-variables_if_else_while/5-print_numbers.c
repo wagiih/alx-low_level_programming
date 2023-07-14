35596d8283a852a10cd1926f7b006f0e01b15bd1
wagiih <m.wagih.raafat@gmail.com>
2023-07-14 12:44:10 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: Always (0) success
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
