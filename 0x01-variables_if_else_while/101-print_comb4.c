652222e304db53e61247696c80da626cbd6a0448
wagiih <m.wagih.raafat@gmail.com>
2023-07-14 18:37:02 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  *main - prints all possible combinations of two digits
+  *Return: 0
+*/
+int main(void)
+{
+	int n, m, i;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (m = n + 1; m <= 9; m++)
+		for (i = m + 1; i <= 9; i++)
+	{
+	putchar(n + '0');
+	putchar(m + '0');
+	putchar(i + '0');
+
+	if (n < 8 || m < 9 || i < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
